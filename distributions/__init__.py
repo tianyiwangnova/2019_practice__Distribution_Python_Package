0f3b877c8cf568f126aee199a7b49ac7a2d40672
Tianyi Wang <tianyi.wang@kindredgroup.com>
2019-11-08 16:09:26 -0500

    add distribution folder

---
+++ distributions/__init__.py
@@ -0,0 +1,5 @@
+from .Gaussiandistribution import Gaussian
+from .Binomialdistribution import Binomial
+from .Generaldistribution import Distribution
+
+# TODO: import the Binomial class from the Binomialdistribution module
